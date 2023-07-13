4978765517812c613067e346fc48fd0528a16206
user228allah <korneeff.ivan@gmail.com>
2023-07-13 20:40:27 +0300

    IO ENGINE

--- IO_EXAMPLE.py
+++ IO_EXAMPLE.py
@@ -1,6 +1,25 @@
+# https://www.pythontutorial.net/python-basics/python-read-text-file/
+
+
 class IOEngine:
     @staticmethod
     def read_text_file():
+        # 'README01.txt
+        # 'README02.txt
+        # 'README03.txt
+
+        # README01 open
+        # README02 open
+        # README03 open
+
+
+        # README03 close
+
+
+        # README02 close
+
+        # README01 close
+
         with open('readme.txt') as f:
             lines = f.readlines()
             f.close()
